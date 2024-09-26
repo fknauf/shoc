756c62b321519c8fd9a3f09d2ea0fd5be51fc02c
Florian Knauf <knauf@vss.uni-hannover.de>
2024-09-26 21:52:10 +0000

    sketch for comch producer

--- doca/comch/producer.cpp
+++ doca/comch/producer.cpp
@@ -1,11 +1,11 @@
-#include "comch_producer.hpp"
+#include "producer.hpp"
 
-#include "logger.hpp"
+#include <doca/logger.hpp>
 
 #include <cassert>
 
-namespace doca {
-    base_comch_producer::base_comch_producer(
+namespace doca::comch {
+    producer::producer(
         context_parent *parent,
         doca_comch_connection *connection,
         std::uint32_t max_tasks
@@ -20,23 +20,25 @@ namespace doca {
 
         enforce_success(doca_comch_producer_task_send_set_conf(
             handle_.handle(),
-            &base_comch_producer::send_completion_entry,
-            &base_comch_producer::send_error_entry,
+            &producer::send_completion_entry,
+            &producer::send_completion_entry,
             max_tasks
         ));
     }
 
-    base_comch_producer::~base_comch_producer() {
+    producer::~producer() {
         assert(get_state() == DOCA_CTX_STATE_IDLE);
     }
 
-    auto base_comch_producer::send(
+    auto producer::send(
         buffer buf,
         std::span<std::uint8_t> immediate_data,
-        std::uint32_t consumer_id,
-        doca_data task_user_data
-    ) -> void {
+        std::uint32_t consumer_id
+    ) -> status_awaitable {
         doca_comch_producer_task_send *task = nullptr;
+
+        auto result = status_awaitable::create_space();
+        doca_data task_user_data = { .ptr = result.dest.get() };
 
         enforce_success(doca_comch_producer_task_send_alloc_init(
             handle_.handle(),
@@ -51,98 +53,28 @@ namespace doca {
         doca_task_set_user_data(base_task, task_user_data);
 
         if(
-            auto err = doca_task_submit(base_task); 
+            auto err = doca_task_submit(base_task);
             err != DOCA_SUCCESS
         ) {
             doca_task_free(base_task);
-            throw doca_exception(err);
-        }
-    }
-
-    auto base_comch_producer::send_completion_entry(
-        [[maybe_unused]] doca_comch_producer_task_send *task,
-        doca_data task_user_data,
-        doca_data ctx_user_data
-    ) -> void {
-        auto base_context = static_cast<context*>(ctx_user_data.ptr);
-        auto producer = static_cast<base_comch_producer*>(base_context);
-
-        if(producer == nullptr) {
-            logger->error("got send completion event without comch_producer");
-        } else {
-            try {
-                producer->send_completion(task, task_user_data);
-            } catch(std::exception &e) {
-                logger->error("comch_producer send completion event handler failed: {}", e.what());
-            } catch(...) {
-                logger->error("comch_producer send completion event handler failed with unknown error");
-            }
+            result.dest->value = err;
         }
 
-        doca_task_free(doca_comch_producer_task_send_as_task(task));
+        return result;
     }
-        
-    auto base_comch_producer::send_error_entry(
+
+    auto producer::send_completion_entry(
         doca_comch_producer_task_send *task,
         doca_data task_user_data,
-        doca_data ctx_user_data
+        [[maybe_unused]] doca_data ctx_user_data
     ) -> void {
-        auto base_context = static_cast<context*>(ctx_user_data.ptr);
-        auto producer = static_cast<base_comch_producer*>(base_context);
+        auto base_task = doca_comch_producer_task_send_as_task(task);
+        auto status = doca_task_get_status(base_task);
 
-        if(producer == nullptr) {
-            logger->error("got send error event without comch_producer");
-        } else {
-            try {
-                producer->send_error(task, task_user_data);
-            } catch(std::exception &e) {
-                logger->error("comch_producer send error event handler failed: {}", e.what());
-            } catch(...) {
-                logger->error("comch_producer send error event handler failed with unknown error");
-            }
-        }
+        doca_task_free(base_task);
 
-        doca_task_free(doca_comch_producer_task_send_as_task(task));
-    }
-
-    auto base_comch_producer::stop() -> void {
-        enforce_success(doca_ctx_stop(as_ctx()), { DOCA_SUCCESS, DOCA_ERROR_IN_PROGRESS });
-    }
-
-    comch_producer::comch_producer(
-        context_parent *parent,
-        doca_comch_connection *connection,
-        std::uint32_t max_tasks,
-        comch_producer_callbacks callbacks
-    ):
-        base_comch_producer { parent, connection, max_tasks },
-        callbacks_ { std::move(callbacks) }
-    {}
-
-    auto comch_producer::state_changed(
-        doca_ctx_states prev_state,
-        doca_ctx_states next_state
-    ) -> void {
-        if(callbacks_.state_changed) {
-            callbacks_.state_changed(*this, prev_state, next_state);
-        }
-    }
-
-    auto comch_producer::send_completion(
-        doca_comch_producer_task_send *task,
-        doca_data task_user_data
-    ) -> void {
-        if(callbacks_.send_completion) {
-            callbacks_.send_completion(*this, task, task_user_data);
-        }
-    }
-
-    auto comch_producer::send_error(
-        doca_comch_producer_task_send *task,
-        doca_data task_user_data
-    ) -> void {
-        if(callbacks_.send_completion) {
-            callbacks_.send_error(*this, task, task_user_data);
-        }
+        auto dest = static_cast<status_awaitable::payload_type*>(task_user_data.ptr);
+        dest->value = status;
+        dest->resume();
     }
 }
--- doca/comch/producer.hpp
+++ doca/comch/producer.hpp
@@ -1,54 +1,40 @@
 #pragma once
 
-#include "buffer.hpp"
-#include "context.hpp"
-#include "memory_map.hpp"
-#include "progress_engine.hpp"
-#include "unique_handle.hpp"
+#include "common.hpp"
+
+#include <doca/buffer.hpp>
+#include <doca/context.hpp>
+#include <doca/memory_map.hpp>
+#include <doca/progress_engine.hpp>
+#include <doca/unique_handle.hpp>
 
 #include <doca_comch_producer.h>
 
 #include <functional>
 
-namespace doca {
-    class base_comch_producer:
+namespace doca::comch {
+    class producer:
         public context
     {
     public:
-        base_comch_producer(
+        producer(
             context_parent *parent,
             doca_comch_connection *connection,
             std::uint32_t max_tasks
         );
 
-        ~base_comch_producer();
+        ~producer();
 
         [[nodiscard]]
         auto as_ctx() const -> doca_ctx* override {
             return doca_comch_producer_as_ctx(handle_.handle());
         }
 
-        auto stop() -> void override;
-
         auto send(
             buffer buf,
             std::span<std::uint8_t> immediate_data,
-            std::uint32_t consumer_id,
-            doca_data task_user_data = { .ptr = nullptr }
-        ) -> void;
-
-    protected:
-        virtual auto send_completion(
-            [[maybe_unused]] doca_comch_producer_task_send *task,
-            [[maybe_unused]] doca_data task_user_data
-        ) -> void {
-        }
-
-        virtual auto send_error(
-            [[maybe_unused]] doca_comch_producer_task_send *task,
-            [[maybe_unused]] doca_data task_user_data
-        ) -> void {
-        }
+            std::uint32_t consumer_id
+        ) -> status_awaitable;
 
     private:
         static auto send_completion_entry(
@@ -56,63 +42,7 @@ namespace doca {
             doca_data task_user_data,
             doca_data ctx_user_data
         ) -> void;
-        
-        static auto send_error_entry(
-            doca_comch_producer_task_send *task,
-            doca_data task_user_data,
-            doca_data ctx_user_data
-        ) -> void;
 
         unique_handle<doca_comch_producer> handle_ { doca_comch_producer_destroy };
-    };
-
-    class comch_producer;
-
-    struct comch_producer_callbacks {
-        using state_changed_callback = std::function<void(
-            comch_producer &self,
-            doca_ctx_states prev_state,
-            doca_ctx_states next_state
-        )>;
-        using send_completion_callback = std::function<void(
-            comch_producer &self, 
-            doca_comch_producer_task_send *task,
-            doca_data task_user_data
-        )>;
-
-        state_changed_callback state_changed = {};
-        send_completion_callback send_completion = {};
-        send_completion_callback send_error = {};
-    };
-
-    class comch_producer:
-        public base_comch_producer
-    {
-    public:
-        comch_producer(
-            context_parent *parent,
-            doca_comch_connection *connection,
-            std::uint32_t max_tasks,
-            comch_producer_callbacks callbacks
-        );
-
-    protected:
-        auto state_changed(
-            doca_ctx_states prev_state,
-            doca_ctx_states next_state
-        ) -> void override;
-
-        virtual auto send_completion(
-            doca_comch_producer_task_send *task,
-            doca_data task_user_data
-        ) -> void override;
-
-        virtual auto send_error(
-            doca_comch_producer_task_send *task,
-            doca_data task_user_data
-        ) -> void override;
-
-    private:
-        comch_producer_callbacks callbacks_;
     };
 }
