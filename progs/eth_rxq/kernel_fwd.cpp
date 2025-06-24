0446a8e6b07b4d4c1c440070d934871bdd28f66f
Florian Knauf <knauf@vss.uni-hannover.de>
2025-06-24 11:35:35 +0000

    kernel-fwd sample

--- progs/eth_rxq/kernel_fwd.cpp
+++ progs/eth_rxq/kernel_fwd.cpp
@@ -5,12 +5,23 @@
 #include <boost/cobalt.hpp>
 #include <cppcodec/hex_lower.hpp>
 
+#include <endian.h>
+
 #include <chrono>
 #include <cstdint>
 #include <iostream>
 #include <vector>
 
 using timer = boost::cobalt::use_op_t::as_default_on_t<boost::asio::steady_timer>;
+
+inline auto be_ipv4_addr(
+    std::uint32_t a,
+    std::uint32_t b,
+    std::uint32_t c,
+    std::uint32_t d
+) -> std::uint32_t {
+    return htobe32((a << 24) | (b << 16) | (c << 8) | d);
+}
 
 auto handle_packets(
     [[maybe_unused]] shoc::progress_engine_lease engine,
@@ -25,7 +36,7 @@ auto handle_packets(
     shoc::logger->info("stopped handling packets: {}", e.what());
 }
 
-auto do_network_stuff(
+auto partial_tap(
     shoc::progress_engine_lease engine,
     char const *ibdev_name
 ) -> boost::cobalt::detached {
@@ -67,23 +78,42 @@ auto do_network_stuff(
 
     shoc::logger->info("RSS started, creating filter pipe...");
 
+    doca_flow_match rss_match = {};
+    rss_match.parser_meta.outer_l4_type = DOCA_FLOW_L4_META_UDP;
+    rss_match.parser_meta.outer_l3_type = DOCA_FLOW_L3_META_IPV4;
+    rss_match.outer.l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_UDP;
+    //rss_match.outer.l3_type = DOCA_FLOW_L3_TYPE_IP4;
+    //rss_match.outer.ip4.src_ip = 0xffffffff;
+    //rss_match.outer.ip4.dst_ip = 0xffffffff;
+    //rss_match.outer.udp.l4_port.src_port = 0xffff;
+    rss_match.outer.udp.l4_port.dst_port = 0xffff;
+
+    auto filter_pipe = shoc::flow::pipe::config { ingress }
+        .set_name("FILTER_PIPE")
+        .set_type(DOCA_FLOW_PIPE_BASIC)
+        .set_is_root(false)
+        .set_match(rss_match)
+        .build(rss->flow_target(), shoc::flow::fwd_kernel{});
+    
+    doca_flow_match entry_rss_match = {};
+    //entry_rss_match.outer.ip4.src_ip = be_ipv4_addr(192, 168, 100, 1);
+    //entry_rss_match.outer.ip4.dst_ip = be_ipv4_addr(192, 168, 100, 218);
+    //entry_rss_match.outer.udp.l4_port.src_port = htobe16(12345);
+    entry_rss_match.outer.udp.l4_port.dst_port = htobe16(12345);
+
+    filter_pipe.add_entry(0, entry_rss_match, std::nullopt, std::nullopt, rss->flow_target(), 0);
+
     doca_flow_match all_match = {};
 
-    doca_flow_actions actions = {};
-    actions.meta.pkt_meta = htobe32(1234);
-    actions.meta.mark = htobe32(5678);
-    doca_flow_actions *actions_idx[] = { &actions };
-
-    auto filter = shoc::flow::pipe::config { ingress }
+    auto root_pipe = shoc::flow::pipe::config { ingress }
         .set_name("ROOT_PIPE")
         .set_type(DOCA_FLOW_PIPE_BASIC)
         .set_is_root(true)
-        .set_domain(DOCA_FLOW_PIPE_DOMAIN_DEFAULT)
         .set_match(all_match)
-        .set_actions(actions_idx)
-        .build(rss->flow_target(), shoc::flow::fwd_drop{});
-    
-    filter.add_entry(0, all_match, actions, std::nullopt, std::monostate{}, 0, nullptr);
+        .build(filter_pipe, shoc::flow::fwd_drop{});
+
+    root_pipe.add_entry(0, all_match, std::nullopt, std::nullopt, filter_pipe, 0);
+
     ingress.process_entries(0, 10ms, 4);
 
     shoc::logger->info("Filter pipe created, will start handling packets now.");
@@ -107,7 +137,7 @@ auto co_main(
     auto engine = shoc::progress_engine{};
     auto env = bluefield_env{};
 
-    do_network_stuff(&engine, env.ibdev_name);
+    partial_tap(&engine, env.ibdev_name);
 
     co_await engine.run();
 }
